c77063dafd13fb427cf07cdbdb2fb8e15bb0c20a
jcapron <elniafron62@gmail.com>
2014-02-07 17:28:02 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedmenow::Application.config.secret_key_base = 'd32b31c4eacd3a9a9c4b2f585a8ea453d55734ed3619ff6adc0f8a11012d42930f3b7d2e2169f519c922e5e0b6e2bf81ecba46a7ab877059c0975701b09c39db'
