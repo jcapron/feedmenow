e1bd6023c9f900e3f081bd7d54d9b2fe9a439dec
jcapron <elniafron62@gmail.com>
2014-02-17 12:16:27 -0500

    changed the path to show users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Feedmenow::Application.routes.draw do
   devise_for :users
   scope "/admin" do
-    resources :users
+    resources :users, except: [:show]
   end
   resources :recipes
 
+  get "users/:id", to: "users#show", as: "show_user"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
