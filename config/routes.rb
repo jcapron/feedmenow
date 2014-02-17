b414fc1430fe57cb154f8f52635c180641072eb5
jcapron <elniafron62@gmail.com>
2014-02-16 19:16:21 -0500

    first model for recipes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Feedmenow::Application.routes.draw do
+  resources :recipes
+
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
